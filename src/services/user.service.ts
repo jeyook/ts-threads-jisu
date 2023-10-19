3b0d0d51005767a9b96bffd7ad9e048cbf86eb25
JISUlicious <ljshong@hanmail.net>
2023-10-19 10:29:28 +0900

    feat: userDao functions created

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,3 +1,4 @@
+import userDao from "../dao/user.dao";
 import { UserInput } from "../types/dataTypes";
 import getIsInputsEmpty from "../utils/getIsInputEmpty";
 import throwError from "../utils/throwError";
@@ -5,6 +6,9 @@ import throwError from "../utils/throwError";
 async function createUser(body: UserInput) {
   const isInputEmpty = getIsInputsEmpty(body.email, body.nickname, body.password);
   if (isInputEmpty) throwError(400, "KEY_ERROR");
+
+  const {password, ...user} = await userDao.createUser(body);
+  return user;
 }
 
 async function signInUser() {}
